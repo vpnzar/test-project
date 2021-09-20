47d1f1919ad0cebcbbabcf5e5a0014bbcffcab19
Vladymyr Nazarenko <vpnazarenko@gmail.com>
2021-09-21 00:50:32 +0300

    fix

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,12 +1,16 @@
+import wordCardTpl from '../templates/translate-card.hbs';
+
 const refs = {
   linkInputField: document.querySelector('#name-input'),
   linkTranslate: document.querySelector('.name-translate'),
   linkExample: document.querySelector('.name-example'),
+  linkCardMarkup: document.querySelector('.search_result'),
+  linkInputText: document.querySelector('#name-input'),
 };
 
 let wordResponse = '';
 
-refs.linkInputField.addEventListener('input', onInputChange);
+refs.linkInputText.addEventListener('input', onInputChange);
 
 const resultFromAPI = async url => {
   const response = await fetch(url);
@@ -15,26 +19,30 @@ const resultFromAPI = async url => {
 };
 
 function onInputChange(event) {
+  console.log(event.currentTarget.value);
   // refs.linkCurrentInputMeaning.textContent = ;
   const eventQuery = event.currentTarget.value;
   resultFromAPI(
     `https://dictionary.skyeng.ru/api/public/v1/words/search?search=${eventQuery}`,
   ).then(text => {
     meaningWords(text[0].meanings[0].id);
+    console.log(text[0].meanings[0].id);
   });
 }
 
 function meaningWords(meaningIds) {
   const meaningURL = fetch(`https://dictionary.skyeng.ru/api/public/v1/meanings?ids=${meaningIds}`)
     .then(meaningURL => meaningURL.json())
-    .then(data => fillOutFields(data[0]));
+    .then(data => renderCollection(data, wordCardTpl));
 }
 
-function fillOutFields(data) {
-  // wordResponse;
-  // refs.linkCurrentInputMeaning.textContent = console.log(data.translation.text);
-  console.log((refs.linkTranslate.textContent = data.translation.text));
-  console.log((refs.linkExample.textContent = data.examples[0].text));
-}
+// function fillOutFields(res) {
+//   ;
+//   // console.log((refs.linkTranslate.textContent = data.translation.text));
+//   // console.log((refs.linkExample.textContent = data.examples[0].text));
+// }
 
 // console.log((refs.linkCurrentInputMeaning.textContent = '111'));
+function renderCollection(arr, renderCard) {
+  arr.forEach(el => refs.linkCardMarkup.insertAdjacentHTML('afterbegin', renderCard(el)));
+}
