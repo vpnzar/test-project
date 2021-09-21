1d72a3e56383bfda88e0b4b9d5c2b8d0da5053ab
unknown <vpnazarenko@gmail.com>
2021-09-21 17:51:16 +0300

    fix

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,4 +1,5 @@
 import wordCardTpl from '../templates/translate-card.hbs';
+import debounce from '../../node_modules/lodash.debounce/index';
 
 const refs = {
   linkInputField: document.querySelector('#name-input'),
@@ -8,26 +9,28 @@ const refs = {
   linkInputText: document.querySelector('#name-input'),
 };
 
-let wordResponse = '';
+// let wordResponse = '';
 
 refs.linkInputText.addEventListener('input', onInputChange);
 
 const resultFromAPI = async url => {
-  const response = await fetch(url);
+  const response = await fetch(
+    `https://dictionary.skyeng.ru/api/public/v1/words/search?search=${url}`,
+  );
   const wordsJson = response.json();
-  return wordsJson;
-};
-
-function onInputChange(event) {
-  console.log(event.currentTarget.value);
-  // refs.linkCurrentInputMeaning.textContent = ;
-  const eventQuery = event.currentTarget.value;
-  resultFromAPI(
-    `https://dictionary.skyeng.ru/api/public/v1/words/search?search=${eventQuery}`,
-  ).then(text => {
+  const wordsJsonData = wordsJson.then(text => {
     meaningWords(text[0].meanings[0].id);
     console.log(text[0].meanings[0].id);
   });
+  return wordsJsonData;
+};
+
+function onInputChange(event) {
+  event.preventDefault();
+  // console.log(event.currentTarget.value);
+  // refs.linkCurrentInputMeaning.textContent = ;
+  const eventQuery = event.currentTarget.value;
+  resultFromAPI(eventQuery);
 }
 
 function meaningWords(meaningIds) {
@@ -36,13 +39,6 @@ function meaningWords(meaningIds) {
     .then(data => renderCollection(data, wordCardTpl));
 }
 
-// function fillOutFields(res) {
-//   ;
-//   // console.log((refs.linkTranslate.textContent = data.translation.text));
-//   // console.log((refs.linkExample.textContent = data.examples[0].text));
-// }
-
-// console.log((refs.linkCurrentInputMeaning.textContent = '111'));
 function renderCollection(arr, renderCard) {
   arr.forEach(el => refs.linkCardMarkup.insertAdjacentHTML('afterbegin', renderCard(el)));
 }
