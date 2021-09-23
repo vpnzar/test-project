7f3224662d78ea3aeadba2b8600aa8d8a6896d64
Vladymyr Nazarenko <vpnazarenko@gmail.com>
2021-09-23 23:12:11 +0300

    1

--- src/js/orders.js
+++ src/js/orders.js
@@ -1,17 +1,34 @@
-import db from '../db.json';
+// import db from '../db.json';
+// let CRM = require('../../node_modules/vtiger/dist/index');
 
-db.displayName;
-console.log(db[0].displayName);
+// db.displayName;
+// console.log(db[0].displayName);
 
-const key = 'Km0Hecx17NF6R7dP';
-const user = 'vpnazarenko@gmail.com';
-const restApi = 'vpnazarenko@gmail.com_Km0Hecx17NF6R7dP';
+// let connection = new CRM.Connection(
+//   'https://know-how.od2.vtiger.com',
+//   'vpnazarenko@gmail.com',
+//   'Km0Hecx17NF6R7dP',
+// );
 
-const resultContactsAPI = async restApi => {
-  const response = await fetch(
-    `https://know-how.od2.odx.vtiger.com/vpnazarenko@gmail.com_Km0Hecx17NF6R7dP/v1/vtiger/default`,
-  );
-  const wordsJson = response.json();
-};
+// connection
+//   .login()
+//   .then(() => {
+//     //login successful
+//   })
+//   .catch(error => {
+//     //catch error
+//     console.error(error.message);
+//   });
 
-resultContactsAPI(restApi);
+// const key = 'Km0Hecx17NF6R7dP';
+// const user = 'vpnazarenko@gmail.com';
+// const restApi = 'vpnazarenko@gmail.com_Km0Hecx17NF6R7dP';
+
+// const resultContactsAPI = async restApi => {
+//   const response = await fetch(
+//     `https://know-how.od2.odx.vtiger.com/vpnazarenko@gmail.com_Km0Hecx17NF6R7dP/v1/vtiger/default`,
+//   );
+//   const wordsJson = response.json();
+// };
+
+// resultContactsAPI(restApi);
